d797d6546518b7c4a2a720dd8b86109421a280fe
MartijnVerlaak <154511539+MartijnVerlaak@users.noreply.github.com>
2026-09-03 11:32:13 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // Vul hier uitsluitend je Spotify Client ID in. Plaats nooit een Client Secret in GitHub.
 window.APP_CONFIG = {
-  SPOTIFY_CLIENT_ID: "e959a423ea8149fb9640fda511b86505
-"
+  SPOTIFY_CLIENT_ID: "e959a423ea8149fb9640fda511b86505"
 };
